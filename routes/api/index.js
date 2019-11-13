1909d2be1ccbdd21064c47083a932ea3ad16256c
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-11-13 00:34:44 -0800

    Added profile routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const tourRoutes = require('./tours');
+const profileRoutes = require('./profile');
 
 router.use('/tours', tourRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const authRoutes = require('./authentication');
+const authRoutes = require('./auth');
 
 router.use('/api', apiRoutes);
 
