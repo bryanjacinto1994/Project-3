de36b0411bcc801bf448fb2726db59508925881b
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-11-12 18:20:21 -0800

    added files for sequelize

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Main from './pages/Main';
 
 class App extends React.Component{
   state={
