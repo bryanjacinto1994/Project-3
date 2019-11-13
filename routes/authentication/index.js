76f777b4e060a2664d6e0714fe24351dff52830e
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-11-13 00:32:16 -0800

    Made a get and post routes for logins auth

--- routes/authentication/index.js
+++ routes/authentication/index.js
@@ -4,3 +4,100 @@ const db = require("../../models");
 
 let returnData = {};
 
+router.get('/user', (req, res) => {
+    // Req has a method that checks for authentication
+    console.log("\nRequest Session")
+    console.log(req.session)
+    if (req.isAuthenticated()) {
+        var currentUser = req.session.passport.user;
+        console.log("\nCurrent User")
+        console.log(currentUser)
+        db.User.findOne({
+            where: {
+                id: currentUser 
+            }
+        }).then(dbUser => {
+            console.log("User")
+            console.log(dbUser)
+            let user = {
+                loggedIn: true,
+                user: currentUser,
+            };
+            res.json(user);
+        })
+
+    } else {
+        let noUser = {
+            loggedIn: false,
+            user: ''
+        };
+        res.json(noUser);
+    }
+})
+
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local-login", (err, user) => {
+        if (err) return next(err)
+
+        if (!user) {
+            returnData = {
+                message: "User not found",
+                color: 'red'
+            }
+            res.json(returnData)
+        }
+
+        req.login(user, (err) => {
+            console.log("\nUser");
+            console.log(user)
+
+            returnData = {
+                message: "User successfully logged in",
+                color: "green"
+            }
+
+            return res.json(returnData)
+        })
+    })(req, res, next)
+})
+
+router.post("/signup", (req, res, next) => {
+    console.log("Hit signup Route")
+    console.log(req.body)
+
+    passport.authenticate("local-signup", (err, user) => {
+        if (err) return next(err)
+
+        if (!user) {
+            console.log("User exists");
+            returnData = {
+                message: "User Exists",
+                color: "red"
+            }
+            return res.json(returnData)
+        }
+
+        if (user) {
+            console.log("\nUser created");
+            returnData = {
+                message: "User Created",
+                color: "green"
+            }
+            res.json(returnData)
+        }
+    })(req, res, next)
+})
+
+router.get("/logout", (req, res) => {
+
+    // Based on that request, we know that the authenticated user is no longer authenticated and is now logged out
+    req.logout()
+    if (!req.user) {
+        res.redirect("/")
+    } else {
+        console.log("Failed Logout")
+    }
+})
+
+
+module.exports = router
