2a366767f1505b5d3f81116af326cdb89f6e7e7c
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-11-12 17:43:43 -0800

    set options for mysql store on condition of process.env

--- server.js
+++ server.js
@@ -15,3 +15,21 @@ require('./config/passport')(passport);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    options = {
+        host: process.env.HOST,
+        port: 3306,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    }
+} else {
+    options = {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: "Lost4815162342",
+        database: 'tracker'
+    }
+}
+
