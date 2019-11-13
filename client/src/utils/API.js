581f7b7b79d01b4194cb80ab3e3e663c50f074aa
Alexei Dias <alexeidias@gmail.com>
2019-11-12 21:36:40 -0800

    added authentication routes in API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,16 @@ const axios = require('axios');
 export default {
     getTours: function () {
         return axios.get('/api/tours')
+    },
+    login: function (creds) {
+        return axios.post("/auth/login", creds)
+    },
+
+    signup: function (creds) {
+        return axios.post("/auth/signup", creds)
+    },
+
+    logout: function () {
+        return axios.get("/auth/logout")
     }
 }
